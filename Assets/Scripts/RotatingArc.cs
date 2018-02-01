173f101dcf0ef4f18f7f0ad60e578fdd34d8b657
chinu3d <chinu3d@yahoo.com>
2018-02-02 02:32:18 +0530

    Level design changes
    Level design changes

---
+++ Assets/Scripts/RotatingArc.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingArc : MonoBehaviour {
+
+    private Rigidbody2D _rigidBody;
+
+	// Use this for initialization
+	void Start () {
+
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Debug.Log(this.gameObject.transform.eulerAngles.z);
+
+        float angleOfRotation = Mathf.Abs(this.gameObject.transform.eulerAngles.z);
+
+        if ((angleOfRotation <= 182.0f) && (angleOfRotation >= 178.0f))
+        {
+            _rigidBody.angularVelocity = 0.0f;
+            StartCoroutine(this.wait());
+        }
+        else
+        {
+            _rigidBody.angularVelocity = -50.0f;
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2.0f);
+        this.gameObject.transform.Rotate(new Vector3(0.0f, 0.0f, -1.0f), 0.1f);
+    }
+}

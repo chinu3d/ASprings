85bbea0b513a39c71966ad41c07ec8d09d0e9cfd
chinu3d <chinu3d@yahoo.com>
2018-02-19 00:22:20 +0530

    Added new levels till level 16
    Added new levels till level 16

--- Assets/Scripts/WindArea.cs
+++ Assets/Scripts/WindArea.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class WindArea : MonoBehaviour {
 
 	public float strength;
-	public Vector3 direction;
+    //public Vector3 direction;
 
-	void OnTriggerEnter2D(Collider2D collision)
+    private Vector3 direction;
+
+    void Start()
+    {
+        //float angleOfRotationAboutZ = this.transform.rotation.z;
+        float angleOfRotationAboutZ = (Mathf.PI/2.0f) - this.transform.rotation.z;
+        this.direction = new Vector3(Mathf.Cos(angleOfRotationAboutZ), Mathf.Sin(angleOfRotationAboutZ), 0.0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
 	{
-		if ((collision.gameObject.tag.Equals("Projectile")) && (collision.gameObject.layer == 13))
-		{
+		
+    }
 
-		}
-		collision.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector3 (direction.x * strength, direction.y * strength, 0.0f));
-	}
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.StartsWith("Projectile"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(direction.x * strength, direction.y * strength, 0.0f));
+        }
+    }
+
 }
